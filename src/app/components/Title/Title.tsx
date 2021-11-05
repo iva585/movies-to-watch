f7d0db6cef5eed69900d23e98761ceddb2c592cd
Walied Rashied <w.rashied@icloud.com>
2021-11-05 11:25:53 +0100

    starting Title component

---
+++ src/app/components/Title/Title.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from 'react';
+import React from 'react';
+import classes from './Title.module.css';
+
+type TitleProps = {
+  children: ReactNode;
+};
+
+function Title({ children }: TitleProps): JSX.Element {
+  return <h1 className={classes.title}>{children}</h1>;
+}
+
+export default Title;
