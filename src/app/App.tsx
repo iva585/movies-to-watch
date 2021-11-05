2efa27360f7c638c45c939b389cbfe1d06708db9
Walied Rashied <w.rashied@icloud.com>
2021-11-05 12:04:33 +0100

    styling title component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
+import Title from './components/Title/Title';
 
-// function App(): JSX.Element {
+function App(): JSX.Element {
+  return <Title>Movies to watch</Title>;
+}
 
-  
-
-// export default App;
+export default App;
