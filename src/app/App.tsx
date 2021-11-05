ccd9a16a234f9b9af95897ed30194c8ebe90515e
Walied Rashied <w.rashied@icloud.com>
2021-11-05 14:44:50 +0100

    create component card

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,55 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
+import Card from './components/Card/Card';
 import Title from './components/Title/Title';
 
 function App(): JSX.Element {
-  return <Title>Movies to watch</Title>;
-}
+  const [cards setCards] = useState([
+    {
+      title: 'Titanic',
+      info: 'romance/drama',
+      watched: false,
+    },
+    {
+      title: 'Godfather',
+      info: 'best movie of all times',
+      watched: true,
+    },
+  ]);
+  
+  function renderCard(
+    cards: {
+      title: string;
+      info: string;
+      watched: boolean;
+    }[]
+  ) {
+
+    return cards.map((card) => {
+      return (
+        <Card
+          key={card.title}
+          title={card.title}
+          info={card.info}
+          watched={card.watched}
+        />
+      );
+    });
+  }
+
+
+  // <Card title={card.title} info={card.info} watched={cards.watched}/>
+
+
+
+
+
+
+  return (
+    <div className={styles.appContainer}>
+      <Title>Movies to watch</Title>
+      {renderCard(cards)}
+      </div>    
+  )}
 
 export default App;
